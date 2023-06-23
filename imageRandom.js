c1e524b9b609ecb227d3b623b27d0e69c64a158b
Anastasiia Kholod <asyaruda06@gmail.com>
2023-06-23 09:32:34 +0300

    g

---
+++ imageRandom.js
@@ -0,0 +1,8 @@
+const images = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg"]
+
+const randomNumber = Math.floor(Math.random() * images.length)
+
+const img = document.createElement("img")
+img.src = "images/" + images[randomNumber]
+
+document.body.appendChild(img)
